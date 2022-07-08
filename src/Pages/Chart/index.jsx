e55b6ef3a58b417dd471f442f8d0ef0bfe073848
wtbno <bruno_links@hotmail.com>
2022-07-07 22:25:18 -0300

    feat.: tuplas das tabelas

--- src/Pages/Chart/index.jsx
+++ src/Pages/Chart/index.jsx
@@ -2,36 +2,123 @@ import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import { Table, Spin } from "antd";
 import NavBar from "../../Components/NavBar";
+
+import { UnorderedListOutlined } from "@ant-design/icons";
 import "./style.css";
 
 export default function TableControl() {
   // Sample Data for the table
   const approvedOrders = [
-    { key: "1", username: "Gourav", age: 10 },
-    { key: "2", username: "Kartik", age: 20 },
-    { key: "3", username: "Madhu", age: 30 },
-    { key: "4", username: "Karu", age: 40 },
-    { key: "5", username: "Dinesh", age: 50 },
+    {
+      key: "1",
+      username: "Gourav",
+      Produtos: 10,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "2",
+      username: "Kartik",
+      Produtos: 20,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "3",
+      username: "Madhu",
+      Produtos: 30,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "4",
+      username: "Karu",
+      Produtos: 40,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "5",
+      username: "Dinesh",
+      Produtos: 50,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
   ];
   const ordersInProcess = [
-    { key: "1", username: "Gourav", age: 10 },
-    { key: "2", username: "Kartik", age: 20 },
-    { key: "3", username: "Madhu", age: 30 },
-    { key: "4", username: "Karu", age: 40 },
-    { key: "5", username: "Dinesh", age: 50 },
+    {
+      key: "1",
+      username: "Gourav",
+      Produtos: 10,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "2",
+      username: "Kartik",
+      Produtos: 20,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "3",
+      username: "Madhu",
+      Produtos: 30,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "4",
+      username: "Karu",
+      Produtos: 40,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
+    {
+      key: "5",
+      username: "Dinesh",
+      Produtos: 50,
+      Valor: "",
+      Quantidade: "",
+      Status: "",
+    },
   ];
 
   // Sample Columns data
   const columns = [
     {
-      title: "Username",
+      title: "Cliente",
       dataIndex: "username",
       key: "username",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Produtos",
+      dataIndex: "Produtos",
+      key: "Produtos",
+    },
+    {
+      title: "Valor",
+      dataIndex: "Valor",
+      key: "Valor",
+    },
+    {
+      title: "Quantidade",
+      dataIndex: "Quantidade",
+      key: "Quantidade",
+    },
+    {
+      title: "Status",
+      dataIndex: "Status",
+      key: "Status",
     },
   ];
 
@@ -51,7 +138,10 @@ export default function TableControl() {
         </div>
       ) : (
         <div>
-          <h4>Monitoramento de pedidos</h4>
+          <h1>
+            <UnorderedListOutlined size={"large"} style={{ margin: "10px" }} />
+            Monitoramento de pedidos
+          </h1>
           <div className="root">
             <div
               style={{
